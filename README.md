7aeb3edf3729a8f66e302b43fe6d85780bfb2356
Kevin McGinty <atomic.frameworks@gmail.com>
2015-11-17 14:38:39 -0500

    Copy other presentation

---
+++ README.md
@@ -0,0 +1,13 @@
+# WebSockets - Effective Developer Series
+
+A presentation on WebSockets.
+
+View the presentation locally by running the commands:
+
+    git clone https://github.com/atomicframeworks/websockets-effective-developer-series.git
+    
+    cd websockets-effective-developer-series
+    
+    npm install
+    
+    grunt serve
---
+++ example/server.js
@@ -0,0 +1,28 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+  res.sendfile('index.html');
+});
+
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+  
+  // When the server receives a chat message emit it back to all sockets
+  socket.on('chat message from client', function (msg) {
+    console.log('server got a message');
+    
+    io.emit('chat message from server', msg);
+
+  });
+  
+});
+
+http.listen(3000, function () {
+  console.log('listening on *:3000');
+});
